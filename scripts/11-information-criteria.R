2790cf9596db26198468bcf0fa82b3d230e38811
Andrew Zieffler <zief0002@umn.edu>
2015-03-12 07:33:22 -0500

    Added scripts

---
+++ scripts/11-information-criteria.R
@@ -0,0 +1,11 @@
+###################################################
+### Write a function (or figure out some functions) to compute
+### AIC
+### AICc
+### Delta
+### w_i
+### Model averaged estimates and SEs
+###################################################
+
+
+
